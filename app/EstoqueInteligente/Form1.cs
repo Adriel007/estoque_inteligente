bd72f27fc7cd8ba1a7497651b1a95449c94f5f2d
Adriel007 <adrielsouzaandrade8@gmail.com>
2025-10-12 17:14:43 -0300

    add: windows form application (untested)

---
+++ app/EstoqueInteligente/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+
+namespace EstoqueInteligente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
